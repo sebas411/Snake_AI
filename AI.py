fdc6c70d8aba5cca14d736d4f41336169a87ec34
Laure30 <gom19501@uvg.edu.gt>
2022-05-19 19:20:29 -0600

    New changes
    New changes

---
+++ AI.py
@@ -0,0 +1,137 @@
+import random
+import json
+
+import dataclasses
+
+@dataclasses.dataclass
+class GameState:
+    distance: tuple
+    position: tuple
+    surroundings: str
+    food: tuple
+
+
+class AI(object):
+    def __init__(self, display_width, display_height, block_size):
+        self.display_width = display_width
+        self.display_height = display_height
+        self.block_size = block_size
+
+        self.epsilon = 0.1
+        self.lr = 0.7
+        self.discount = .5
+
+        self.qvalues = self.LoadQvalues()
+        self.history = []
+
+        self.actions = {
+            0:'left',
+            1:'right',
+            2:'up',
+            3:'down'
+        }
+
+    def Reset(self):
+        self.history = []
+
+    def LoadQvalues(self, path="qvalues.json"):
+        with open(path, "r") as f:
+            qvalues = json.load(f)
+        return qvalues
+
+    def SaveQvalues(self, path="qvalues.json"):
+        with open(path, "w") as f:
+            json.dump(self.qvalues, f)
+            
+    def act(self, snake, food):
+        state = self._GetState(snake, food)
+
+        rand = random.uniform(0,1)
+        if rand < self.epsilon:
+            action_key = random.choices(list(self.actions.keys()))[0]
+        else:
+            state_scores = self.qvalues[self._GetStateStr(state)]
+            action_key = state_scores.index(max(state_scores))
+        action_val = self.actions[action_key]
+        
+        self.history.append({
+            'state': state,
+            'action': action_key
+            })
+        return action_val
+    
+    def UpdateQValues(self, reason):
+        history = self.history[::-1]
+        for i, h in enumerate(history[:-1]):
+            if reason: 
+                sN = history[0]['state']
+                aN = history[0]['action']
+                state_str = self._GetStateStr(sN)
+                reward = -1
+                self.qvalues[state_str][aN] = (1-self.lr) * self.qvalues[state_str][aN] + self.lr * reward 
+                reason = None
+            else:
+                s1 = h['state'] 
+                s0 = history[i+1]['state'] 
+                a0 = history[i+1]['action'] 
+                
+                x1 = s0.distance[0] 
+                y1 = s0.distance[1] 
+    
+                x2 = s1.distance[0] 
+                y2 = s1.distance[1]
+                
+                if s0.food != s1.food:
+                    reward = 1
+                elif (abs(x1) > abs(x2) or abs(y1) > abs(y2)): 
+                    reward = 1
+                else:
+                    reward = -1 
+                    
+                state_str = self._GetStateStr(s0)
+                new_state_str = self._GetStateStr(s1)
+                self.qvalues[state_str][a0] = (1-self.lr) * (self.qvalues[state_str][a0]) + self.lr * (reward + self.discount*max(self.qvalues[new_state_str])) 
+
+
+    def _GetState(self, snake, food):
+        snake_head = snake[-1]
+        dist_x = food[0] - snake_head[0]
+        dist_y = food[1] - snake_head[1]
+
+        if dist_x > 0:
+            pos_x = '1'
+        elif dist_x < 0:
+            pos_x = '0'
+        else:
+            pos_x = 'NA' 
+
+        if dist_y > 0:
+            pos_y = '3'
+        elif dist_y < 0:
+            pos_y = '2'
+        else:
+            pos_y = 'NA' 
+
+        sqs = [
+            (snake_head[0]-self.block_size, snake_head[1]),   
+            (snake_head[0]+self.block_size, snake_head[1]),         
+            (snake_head[0],                  snake_head[1]-self.block_size),
+            (snake_head[0],                  snake_head[1]+self.block_size),
+        ]
+        
+        surrounding_list = []
+        for sq in sqs:
+            if sq[0] < 0 or sq[1] < 0: 
+                surrounding_list.append('1')
+            elif sq[0] >= self.display_width or sq[1] >= self.display_height: 
+                surrounding_list.append('1')
+            elif sq in snake[:-1]:
+                surrounding_list.append('1')
+            else:
+                surrounding_list.append('0')
+        surroundings = ''.join(surrounding_list)
+
+        return GameState((dist_x, dist_y), (pos_x, pos_y), surroundings, food)
+
+    def _GetStateStr(self, state):
+        return str((state.position[0],state.position[1],state.surroundings))
--- snake.py
+++ snake.py
@@ -2,23 +2,35 @@
 
 import pygame
 import random
-import Learner
+import AI
 
 pygame.init()
 
 YELLOW = (255, 255, 102)
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
-BLUE = (50, 153, 213)
+BLUE = (20, 13, 213)
+RED = (255, 0, 0)
+
 
 BLOCK_SIZE = 10 
 DIS_WIDTH = 600
 DIS_HEIGHT = 400
 
-QVALUES_N = 100
-FRAMESPEED = 50000
+QVALUES_N = 50
+FRAMESPEED = 3000
 
+def DrawFood(foodx, foody):
+    pygame.draw.rect(dis, RED, [foodx, foody, BLOCK_SIZE, BLOCK_SIZE])   
 
+def DrawScore(score):
+    font = pygame.font.SysFont("arial", 35)
+    value = font.render(f"Score: {score}", True, BLACK)
+    dis.blit(value, [0, 365])
+
+def DrawSnake(snake_list):
+    for x in snake_list:
+        pygame.draw.rect(dis, BLACK, [x[0], x[1], BLOCK_SIZE, BLOCK_SIZE])
 
 def GameLoop():
     global dis
@@ -27,7 +39,6 @@ def GameLoop():
     pygame.display.set_caption('Snake')
     clock = pygame.time.Clock()
 
-    # Starting position of snake
     x1 = DIS_WIDTH / 2
     y1 = DIS_HEIGHT / 2
     x1_change = 0
@@ -35,14 +46,12 @@ def GameLoop():
     snake_list = [(x1,y1)]
     length_of_snake = 1
 
-    # Create first food
     foodx = round(random.randrange(0, DIS_WIDTH - BLOCK_SIZE) / 10.0) * 10.0
     foody = round(random.randrange(0, DIS_HEIGHT - BLOCK_SIZE) / 10.0) * 10.0
 
     dead = False
     reason = None
     while not dead:
-        # Get action from agent
         action = learner.act(snake_list, (foodx,foody))
         if action == "left":
             x1_change = -BLOCK_SIZE
@@ -57,73 +66,59 @@ def GameLoop():
             y1_change = BLOCK_SIZE
             x1_change = 0
 
-        # Move snake
         x1 += x1_change
         y1 += y1_change
         snake_head = (x1,y1)
         snake_list.append(snake_head)
 
-        # Check if snake is off screen
         if x1 >= DIS_WIDTH or x1 < 0 or y1 >= DIS_HEIGHT or y1 < 0:
-            reason = 'Screen'
+            reason = 'Bordes'
             dead = True
 
-        # Check if snake hit tail
         if snake_head in snake_list[:-1]:
-            reason = 'Tail'
+            reason = 'Cola'
             dead = True
 
-        # Check if snake ate food
         if x1 == foodx and y1 == foody:
             foodx = round(random.randrange(0, DIS_WIDTH - BLOCK_SIZE) / 10.0) * 10.0
             foody = round(random.randrange(0, DIS_HEIGHT - BLOCK_SIZE) / 10.0) * 10.0
             length_of_snake += 1
 
-        # Delete the last cell since we just added a head for moving, unless we ate a food
         if len(snake_list) > length_of_snake:
             del snake_list[0]
 
-        # Draw food, snake and update score
         dis.fill(BLUE)
         DrawFood(foodx, foody)
         DrawSnake(snake_list)
         DrawScore(length_of_snake - 1)
         pygame.display.update()
 
-        # Update Q Table
         learner.UpdateQValues(reason)
         
-        # Next Frame
         clock.tick(FRAMESPEED)
 
     return length_of_snake - 1, reason
 
-def DrawFood(foodx, foody):
-    pygame.draw.rect(dis, GREEN, [foodx, foody, BLOCK_SIZE, BLOCK_SIZE])   
-
-def DrawScore(score):
-    font = pygame.font.SysFont("comicsansms", 35)
-    value = font.render(f"Score: {score}", True, YELLOW)
-    dis.blit(value, [0, 0])
-
-def DrawSnake(snake_list):
-    for x in snake_list:
-        pygame.draw.rect(dis, BLACK, [x[0], x[1], BLOCK_SIZE, BLOCK_SIZE])
 
 
 game_count = 1
 
-learner = Learner.Learner(DIS_WIDTH, DIS_HEIGHT, BLOCK_SIZE)
+learner = AI.AI(DIS_WIDTH, DIS_HEIGHT, BLOCK_SIZE)
+
+epsi_range = (0.5, 0)
+max_e_iter = 100
 
 while True:
     learner.Reset()
-    if game_count > 100:
+    if game_count > max_e_iter:
         learner.epsilon = 0
     else:
-        learner.epsilon = .1
+        new_e = (epsi_range[0]-epsi_range[1])*(max_e_iter - game_count)/max_e_iter + epsi_range[1]
+        print(new_e)
+        learner.epsilon = new_e
     score, reason = GameLoop()
-    print(f"Games: {game_count}; Score: {score}; Reason: {reason}") # Output results of each game to console to monitor as agent is training
+    print(f"No. iter: {game_count}; Puntaje: {score}; Error: {reason}") 
     game_count += 1
-    if game_count % QVALUES_N == 0: # Save qvalues every qvalue_dump_n games
-        print("Save Qvals")
+    if game_count % QVALUES_N == 0:
+        print("Guardar tabla Q")
         learner.SaveQvalues()
